b16820506ca33c21c4be0c0884ba2ee47b1d1cd4
Desmund Weathers <desmundweathers@gmail.com>
2016-09-29 12:35:19 -0500

    removed redundant folder

---
+++ Code2040/Code2040/Step 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace Code2040
+{
+    class Step_2
+    {
+        static void Main(string[] args)
+        {
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://challenge.code2040.org/api/reverse");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using(var streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    string givenstring = streamReader.ReadToEnd();
+                    Console.WriteLine(givenstring);
+                }
+
+                
+
+                    }
+                }
+            }
+
+        }
+    
+
+
---
+++ Step 2/Step 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace Step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://challenge.code2040.org/api/reverse");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{\"token\" :\"c542488ecdbab538ee07b0383f7d7af3\"}";
+                              
+            
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+            }
+
+            
+        }
+
+
+    }
+}
+    
+
