11efdcc8c6de95916d680725d5052b5ab087910a
dweathers <dweathers@HDS-DS8RLS1>
2016-10-18 17:17:16 -0500

    Initial

--- Step 3/Program.cs
+++ Step 3/Program.cs
@@ -11,6 +11,46 @@ namespace Step_3
     {
         static void Main(string[] args)
         {
+            var data = makeRequest("http://challenge.code2040.org/api/haystack", "{\"token\" :\"c542488ecdbab538ee07b0383f7d7af3\"}");
+            Console.WriteLine(data);
+
+
+
+            string reversestring = "";
+            char[] cArray = data.ToCharArray();
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reversestring += cArray[i];
+            }
+
+            var json = "{\"token\" :\"c542488ecdbab538ee07b0383f7d7af3\"," + "\"string\":\"" + reversestring + "\"}";
+            Console.WriteLine(json);
+            var newdata = makeRequest("http://challenge.code2040.org/api/haystack/validate", json);
+            Console.WriteLine(newdata);
+
+
+        }
+
+
+        public static string makeRequest(string url, string json)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+            }
+            
+
+            var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                json = streamReader.ReadToEnd();
+            }
+            return json;
+
         }
     }
 }
